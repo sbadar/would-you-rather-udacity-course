a615d39cae49f10a1749aa7287af9410f2556b2b
sbadar <35875549+sbadar@users.noreply.github.com>
2019-12-06 01:44:40 -0500

    New Repository

---
+++ src/middleware/logger.js
@@ -0,0 +1,12 @@
+const logger = (store) => (next) => (action) =>{
+    console.group(action.type);
+    console.log('The action ' , action);
+    const resultValue = next(action);
+    console.log('The new state is ', store.getState());
+    console.groupEnd();
+
+    return resultValue;
+
+}
+
+export default logger;
---
+++ src/utils/api.js
@@ -0,0 +1,14 @@
+import {
+    _getUsers,
+    _getQuestions,
+  } from '../utils/_Data'
+  
+  export function getInitialData () {
+    return Promise.all([
+      _getUsers(),
+      _getQuestions(),
+    ]).then(([users, questions]) => ({
+      users,
+      questions,
+    }))
+  }
